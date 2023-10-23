06324a280baa0eab7fc26596496e804ed9cd7d97
Vinicius-Almeida-Dev <viniciussalne@hotmail.com>
2023-10-23 18:46:58 -0300

    Projeto terminado, fiz uma brincadeira kkk

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
 
 namespace Projeto_Aula_201
 {
@@ -10,6 +9,33 @@ namespace Projeto_Aula_201
     {
         static void Main(string[] args)
         {
+            string sourceFile = @"C:\Windows\Temp\soucerFile.csv";
+            string targetFolder = @"C:\É-O-VINI-AMIGO";
+
+            try
+            {
+                Directory.CreateDirectory($"{targetFolder}");
+
+
+                using (StreamWriter sw = File.AppendText($@"{targetFolder}\Summary.txt"))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        string laEle = $@" lá ele {i} -  lá ele {i} - lá ele {i} -  lá ele {i}";
+                        sw.WriteLine(laEle.ToUpper());
+
+                        Console.WriteLine("Caiu no bait BB");
+                    }
+                }
+
+                Console.ReadLine();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
